f72c5655c99f5595713eb0d12a72a9444fe83f78
Tamara Gjorgieva <tamara_gjorgieva@hotmail.com>
2018-11-11 19:17:22 +0100

    Updated code_LV01

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
 import './App.css';
+import EditStudentDetails from '../EditStudentDetails/EditStudentDetails';
+import StudentsList from '../StudentsList/StudentsList';
+import listStudents from '../../repository/studentRepository';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {students:listStudents()};
+    }
+
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        let obj={
+            firstName: event.target[0].value,
+            lastName:event.target[1].value,
+            index: event.target[2].value,
+            direction: event.target[3].value,
+            key:event.target[2].value};
+        event.target[0].value="";
+        event.target[1].value="";
+        event.target[2].value="";
+        event.target[3].value="";
+        let arr=this.state.students;
+        arr.push(obj);
+        this.setState({students:arr});
+    };
+
+
+
+
+    render() {
+
+        return ([
+
+            <div className="container">
+                <EditStudentDetails handleSubmitCB={this.handleSubmit}  /> ,
+            <table   className={'table table-striped table-bordered mt-5 text-center mw-100'}>
+                <thead>
+                <tr>
+                   <th>FirstName</th>
+                    <th>LastName</th>
+                    <th>Index</th>
+                    <th>Direction</th>
+                    <th>Show more details</th>
+                </tr>
+                </thead>
+                <tbody>
+                <StudentsList students={this.state.students}/>
+                </tbody>
+            </table>
+            </div>
+        ]);
+    }
 }
 
 export default App;
---
+++ src/repository/studentRepository.js
@@ -0,0 +1,28 @@
+export const listStudents = () => {
+    return [
+        {
+            firstName: "Tamara",
+            lastName:"Gjorgieva",
+            index: "163004",
+            direction: "PET"},
+        {
+            firstName: "Ana",
+            lastName:"Anevska",
+            index: "123012",
+            direction: "KNI"},
+        {
+            firstName: "Dimitar ",
+            lastName:"Dimitrov",
+            index: "173127",
+            direction: "MT"},
+        {
+            firstName: "Stefan",
+            lastName:"Stefanovski",
+            index: "151235",
+            direction: "KNI"}
+
+
+
+    ];
+};
+export default listStudents;
